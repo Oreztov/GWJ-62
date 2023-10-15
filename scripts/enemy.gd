e2e6630f32c1ad509dc06650a2713d4f1917bd5d
Oreztov <shabugi@outlook.com>
2023-10-15 15:24:21 +0300

    NavigationAgent enemy works

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,16 +1,31 @@
 extends CharacterBody2D
 
 
-@export var speed = 10
-@onready var nav_agent = $NavNode/NavigationAgent2D
+@export var speed = 100
+@onready var nav_agent = $NavigationAgent2D
 
+var next_pos
 
 func ready():
 	pass
 
 func _physics_process(delta):
-	var direction = to_local(nav_agent.get_next_path_position())
-	velocity = direction * speed
+	if nav_agent.is_navigation_finished():
+		set_velocity(Vector2.ZERO)
+		move_and_slide()
+		return
+		
+	nav_agent.target_position = Globals.player_reference.global_position
+	# Calculate and set agent velocity towards next waypoint
+	var current_agent_position: Vector2 = global_transform.origin
+	var next_path_position: Vector2 = nav_agent.get_next_path_position()
+
+	var new_velocity: Vector2 = next_path_position - current_agent_position
+	new_velocity = new_velocity.normalized()
+	new_velocity = new_velocity * speed
+	
+	set_velocity(new_velocity)
+	
 	move_and_slide()
 	
 func find_path():
