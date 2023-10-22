a06b52d394ae9e354b29420a565a7d43d6f052f8
Oreztov <shabugi@outlook.com>
2023-10-22 19:35:13 +0300

    The Game Build

--- scripts/UI/colorblind_rect.gd
+++ scripts/UI/colorblind_rect.gd
@@ -4,8 +4,6 @@ extends ColorRect
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Globals.colorblind_reference = self
-	if not OS.has_feature("web"):
-		update()
 
 func update():
 	material.set_shader_parameter("mode", Globals.colorblind_mode)
